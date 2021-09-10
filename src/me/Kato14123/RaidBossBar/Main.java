fe68ae0f856bffb3f958efad667a7dc071f4adf7
katoretb <katoretv@gmail.com>
2021-09-10 08:25:08 +0700

    1.1.1

---
+++ src/me/Kato14123/RaidBossBar/Main.java
@@ -0,0 +1,40 @@
+package me.Kato14123.RaidBossBar;
+
+import java.time.LocalDateTime;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+	
+	public Bar bar;
+	
+	@Override
+	public void onEnable(){
+		LocalDateTime now = LocalDateTime.now();
+		if(String.valueOf(now.getDayOfWeek()) == "SUNDAY") {
+			this.getServer().getPluginManager().registerEvents(this, this);
+			bar = new Bar(this);
+			bar.createBar();		
+			if (Bukkit.getOnlinePlayers().size() > 0)
+				for (Player on : Bukkit.getOnlinePlayers())
+					bar.addPlayer(on);
+		}
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		if (!bar.getBar().getPlayers().contains(event.getPlayer()))
+			bar.addPlayer(event.getPlayer());
+	}
+	
+}
